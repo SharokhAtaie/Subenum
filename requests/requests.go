1b297df521b82d3a1875e4d7c9ecc05b4488b16b
Sharo_k_h <94289484+SharokhAtaie@users.noreply.github.com>
2022-12-17 09:54:49 +0330

    Update requests.go

--- requests/requests.go
+++ requests/requests.go
@@ -47,17 +47,6 @@ func JldcMe(URL string) string {
 	return string(resBody)
 }
 
-func OmnisintIo(URL string) string {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", URL, nil)
-	res, err := client.Do(req)
-	if err != nil {
-		fmt.Println("can't requet to url", err)
-	}
-	defer res.Body.Close()
-	resBody, _ := ioutil.ReadAll(res.Body)
-	return string(resBody)
-}
 
 func AbuseDB(URL string) string {
 	client := &http.Client{}
